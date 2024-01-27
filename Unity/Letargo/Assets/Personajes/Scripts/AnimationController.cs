523a13cc75966665e948d24c2cb463adbcb6aa8e
Rafi k <rafamorenoriquelme2004@gmail.com>
2024-01-27 16:34:54 +0100

    Cuando pulsas B el personaje baila

--- Unity/Letargo/Assets/Personajes/Scripts/AnimationController.cs
+++ Unity/Letargo/Assets/Personajes/Scripts/AnimationController.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class AnimationController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Referencia al componente de animación
+    private Animator animator;
+
     void Start()
     {
-        
+        // Obtener el componente Animator adjunto al mismo GameObject
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            // Verificar si la tecla "B" está siendo presionada
+            Debug.Log("Se presionó la tecla 'b'");
+
+            // Ejecutar la animación "Rumba Dancing"
+            animator.SetTrigger("RumbaDancingTrigger");
+        }
     }
 }
