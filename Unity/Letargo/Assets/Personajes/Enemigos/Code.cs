1c00125ce04ff5b0c087199badc41bb931c54b91
verokayt <verbascel@alu.edu.gva.es>
2024-02-06 10:44:56 +0100

    Run y seguimiento al personaje principal

--- Unity/Letargo/Assets/Personajes/Enemigos/Code.cs
+++ Unity/Letargo/Assets/Personajes/Enemigos/Code.cs
@@ -10,45 +10,83 @@ public class Code : MonoBehaviour
     public Quaternion angulo;
     public float grado;
 
+    public GameObject target;
+    public bool atacando;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        ani = GetComponent<Animator>(); 
+        ani = GetComponent<Animator>();
+        target = GameObject.Find("PlayerArmature");
     }
 
     public void Comportamiento_Enemigo()
     {
-        cronometro += 1 * Time.deltaTime;
-        if (cronometro >= 4)
+        if (Vector3.Distance(transform.position, target.transform.position) > 10)
         {
-            rutina = Random.Range(0, 2);
-            cronometro = 0;
-         }
-        switch (rutina)
+            ani.SetBool("run", false);
+            cronometro += 1 * Time.deltaTime;
+            if (cronometro >= 4)
+            {
+                rutina = Random.Range(0, 2);
+                cronometro = 0;
+            }
+            switch (rutina)
+            {
+                case 0:
+                    ani.SetBool("walk", false);
+                    Debug.Log("se esta enviando false");
+                    break;
+
+                case 1:
+                    grado = Random.Range(0, 360);
+                    angulo = Quaternion.Euler(0, grado, 0);
+                    rutina++;
+                    break;
+
+                case 2:
+                    transform.rotation = Quaternion.RotateTowards(transform.rotation, angulo, 0.5f);
+                    transform.Translate(Vector3.forward * 1 * Time.deltaTime);
+                    ani.SetBool("walk", true);
+                    break;
+            }
+        }
+        else
         {
-            case 0:
+            if (Vector3.Distance(transform.position, target.transform.position) > 1 && !atacando)
+            {
+                var lookPos = target.transform.position - transform.position;
+                lookPos.y = 0;
+                var rotation = Quaternion.LookRotation(lookPos);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 2);
                 ani.SetBool("walk", false);
-                Debug.Log("se esta enviando false");
-                break;
 
-            case 1:
-                grado = Random.Range(0, 360);
-                angulo = Quaternion.Euler(0, grado, 0);
-                rutina++;
-                break;
+                ani.SetBool("run", true);
+                transform.Translate(Vector3.forward * 2 * Time.deltaTime);
 
-            case 2:
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, angulo, 0.5f);
-                transform.Translate(Vector3.forward * 1 * Time.deltaTime);
-                ani.SetBool("walk", true);
-                break;
-             }
+                ani.SetBool("attack", false);
+            }
+            else
+            {
+                ani.SetBool("walk", false);
+                ani.SetBool("run", false);
+
+                ani.SetBool("attack", true);
+                atacando = true;
+            }
 
         }
-
+    }
+    public void Final_Ani()
+    {
+        ani.SetBool("attack", false);
+        atacando = false;
+    }
     // Update is called once per frame
     void Update()
     {
         Comportamiento_Enemigo();
     }
 }
+
