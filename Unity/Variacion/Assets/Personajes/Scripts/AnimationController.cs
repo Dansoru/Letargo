ba970e95dfab982cc2933234131a5e237b771b9e
verokayt <verbascel@alu.edu.gva.es>
2024-02-09 09:05:59 +0100

    Letargo nuevo

---
+++ Unity/Variacion/Assets/Personajes/Scripts/AnimationController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    // Referencia al componente de animación
+    private Animator animator;
+
+    void Start()
+    {
+        // Obtener el componente Animator adjunto al mismo GameObject
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            // Verificar si la tecla "B" está siendo presionada
+            Debug.Log("Se presionó la tecla 'b'");
+
+            // Ejecutar la animación "Rumba Dancing"
+            animator.SetTrigger("RumbaDancingTrigger");
+        }
+
+      
+        if (Input.GetMouseButtonDown(0)) // 0 representa el botón izquierdo del ratón
+        {
+            // Verificar si se presionó click izquierdo
+            Debug.Log("Se presionó click izquierdo");
+
+            // Ejecutar la animación "punching"
+            animator.SetTrigger("PunchingTrigger");
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            // Verificar si se presionó Control
+            Debug.Log("Se presionó Control Izquierdo");
+
+            // Ejecutar la animación "flip"  
+            animator.SetTrigger("FlipTrigger");
+        }
+
+    }
+}
+
+
+
+
