30ade345f9215699ffdec8a257cb60cc00ef5f82
verokayt <verbascel@alu.edu.gva.es>
2024-02-10 16:18:33 +0100

    Barra de vida( prototipo)

--- Unity/Variacion/Assets/Personajes/Scripts/DatosJugador.cs
+++ Unity/Variacion/Assets/Personajes/Scripts/DatosJugador.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class DatosJugador : MonoBehaviour
 
 {
     public int vidaPlayer;
-
+    public Slider vidaVisual;
        
         private void Update()
         {
+
+
+        vidaVisual.GetComponent<Slider>().value = vidaPlayer;
            if(vidaPlayer <=0)
            {
             Debug.Log("Game over");
