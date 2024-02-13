75a69e2d78d3d7e1bfcc602d579329e4eb7cea64
verokayt <verbascel@alu.edu.gva.es>
2024-02-13 12:05:27 +0100

    Sistema de vida

---
+++ Unity/Variacion/Assets/GUI/Scripts/Corazones.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Corazones : MonoBehaviour
+{
+    public Sprite[] corazones;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        CambioVida(3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    public void CambioVida(int pos)
+    {
+        this.GetComponent<Image>().sprite = corazones [pos];
+    }
+
+}
