fd83f7521dc44a4bd240b285f09e661a1dfd102b
verokayt <verbascel@alu.edu.gva.es>
2024-02-16 12:50:01 +0100

    .

--- Unity/Variacion 2.0/Assets/Personajes/Scripts/Code.cs
+++ Unity/Variacion 2.0/Assets/Personajes/Scripts/Code.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
 public class Code : MonoBehaviour
 {
@@ -14,10 +13,7 @@ public class Code : MonoBehaviour
     public GameObject target;
     public bool atacando;
 
-    public NavMeshAgent agente;
-    public float distancia_ataque;
-    public float radio_vision;
-    public float speed; // Agregar variable speed
+    
 
     // Start is called before the first frame update
     void Start()
@@ -25,19 +21,17 @@ public class Code : MonoBehaviour
         ani = GetComponent<Animator>();
         target = GameObject.Find("PlayerArmature");
     }
-
-    void Update()
+     void Update()
     {
         Comportamiento_Enemigo();
     }
 
     public void Comportamiento_Enemigo()
     {
-        if (Vector3.Distance(transform.position, target.transform.position) > radio_vision)
+        if (Vector3.Distance(transform.position, target.transform.position) > 10)
         {
-            agente.enabled = false;
             ani.SetBool("run", false);
-            cronometro += Time.deltaTime;
+            cronometro += 1 * Time.deltaTime;
             if (cronometro >= 4)
             {
                 rutina = Random.Range(0, 2);
@@ -59,48 +53,39 @@ public class Code : MonoBehaviour
 
                 case 2:
                     transform.rotation = Quaternion.RotateTowards(transform.rotation, angulo, 0.5f);
-                    transform.Translate(Vector3.forward * speed * Time.deltaTime); // Cambiar "speed" por "agente.speed"
+                    transform.Translate(Vector3.forward * 1 * Time.deltaTime);
                     ani.SetBool("walk", true);
                     break;
             }
         }
         else
         {
-            var lookPos = target.transform.position - transform.position;
-            lookPos.y = 0;
-            var rotation = Quaternion.LookRotation(lookPos);
-
-            agente.enabled = true;
-            agente.SetDestination(target.transform.position);
-
-            if (Vector3.Distance(transform.position, target.transform.position) > distancia_ataque && !atacando)
+            if (Vector3.Distance(transform.position, target.transform.position) > 1 && !atacando)
             {
+                var lookPos = target.transform.position - transform.position;
+                lookPos.y = 0;
+                var rotation = Quaternion.LookRotation(lookPos);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 2);
                 ani.SetBool("walk", false);
+
                 ani.SetBool("run", true);
+                transform.Translate(Vector3.forward * 3 * Time.deltaTime);
+
+                ani.SetBool("attack", false);
             }
             else
             {
-                if (!atacando)
-                {
-                    transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 1);
-                    ani.SetBool("walk", false);
-                    ani.SetBool("run", false);
-                }
-            }
-        }
+                ani.SetBool("walk", false);
+                ani.SetBool("run", false);
 
-        if (atacando)
-        {
-            agente.enabled = false;
+                ani.SetBool("attack", true);
+                atacando = true;
+            }
+
         }
     }
-
     public void Final_Ani()
     {
-        if (Vector3.Distance(transform.position, target.transform.position) > distancia_ataque + 0.2f)
-        {
-            ani.SetBool("attack", false);
-        }
         ani.SetBool("attack", false);
         atacando = false;
         Debug.Log("esta reaccionando");
@@ -112,4 +97,5 @@ public class Code : MonoBehaviour
     {
         Final_Ani();
     }
+
 }
